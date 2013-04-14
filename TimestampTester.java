c1ade5d70994373a57be50a8ac1f13eff54a031c
Alfandorachk <spiffpneumoyakk@gmail.com>
2013-04-13 22:30:09 -0400

    Made Timestamp comparable

--- TimestampTester.java
+++ TimestampTester.java
@@ -8,6 +8,30 @@ public class TimestampTester {
         BufferedReader stdIn = new BufferedReader( new InputStreamReader(
                                                     System.in));
         Timestamp ts;
+        Timestamp other_ts;
+
+        other_ts = new Timestamp();
+        ts = new Timestamp();
+
+        int difference = other_ts.compareTo(ts);
+        String diff;
+        switch (difference) {
+        case 0:
+            diff = "the same as";
+            break;
+        case 1:
+            diff = "greater than";
+            break;
+        case -1:
+            diff = "less than";
+            break;
+        default:
+            diff = "well, crap";
+            break;
+        }
+       
+
+        System.out.printf("other_ts is %s ts\n", diff); 
 
         System.out.print("Hit enter to get the time; ");
         System.out.print("or exit to quit.\n");
--- bitter/util/Timestamp.java
+++ bitter/util/Timestamp.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 
-public class Timestamp {
+public class Timestamp implements Comparable<Timestamp> {
 
     private static final String format = "EEE, yyyy.MM.dd 'at' HH:mm z";
     private static final DateFormat formatter = new SimpleDateFormat(format);
@@ -15,12 +15,20 @@ public class Timestamp {
         time = new Date();
     }
 
+    public Timestamp(Date date) {
+        time = date;
+    }
+
     public Date getStamp() {
         return time;
     }
 
     public String toPrintableString() {
         return formatter.format(time);
+    }
+
+    public int compareTo(Timestamp ts) {
+        return this.time.compareTo(ts.time);
     }
 
 } // End class Timestamp
