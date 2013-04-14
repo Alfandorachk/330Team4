31320e088755ee23601ff968a16f176b1cc693f9
Alfandorachk <spiffpneumoyakk@gmail.com>
2013-04-13 22:35:52 -0400

    Commented timestamp

--- bitter/util/Timestamp.java
+++ bitter/util/Timestamp.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 
+/**
+ * Provides an easy-to-use timestamp with a standard output format.
+ */
 public class Timestamp implements Comparable<Timestamp> {
 
     private static final String format = "EEE, yyyy.MM.dd 'at' HH:mm z";
@@ -11,22 +14,43 @@ public class Timestamp implements Comparable<Timestamp> {
     
     private Date time;
 
+    /**
+     * Create a new timestamp based on the current time.
+     */
     public Timestamp() {
         time = new Date();
     }
 
+    /**
+     * Create a new timestamp based on the given Date.
+     */
     public Timestamp(Date date) {
         time = date;
     }
 
-    public Date getStamp() {
+    /**
+     * Returns a Date object with the same time value as the Timestamp.
+     * @return a Date object with the same value as the timestamp
+     */
+    public Date toDate() {
         return time;
     }
 
+    /**
+     * Returns a pretty string representing the time of the timestamp.
+     * @return a print-worthy string representing the time of the timestamp
+     */
     public String toPrintableString() {
         return formatter.format(time);
     }
 
+    /**
+     * Compares this timestamp to another.
+     * @param ts the timestamp to be compared to
+     * @return -1, 0, or 1 if this timestamp represents a time earlier than,
+     *          equal to, or greater than the given time, respectively
+     * @see java.util.Date#compareTo
+     */
     public int compareTo(Timestamp ts) {
         return this.time.compareTo(ts.time);
     }
