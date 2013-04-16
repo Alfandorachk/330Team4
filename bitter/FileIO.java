2094a790fc9ec4e2b4bf17635c3376df83ac39aa
Peter <peter@peter-Inspiron-N5010.(none)>
2013-04-16 09:47:16 -0400

    revamped the file, still having some issues

--- bitter/FileIO.java
+++ bitter/FileIO.java
@@ -1,14 +1,30 @@
 package bitter;
 
 import java.io.*;
+import java.nio.*;
+import java.nio.charset.Charset;
 
-
-public class FileIO {
-	private File file;
+public class FileIO  interface Path {
+	final public Charset CHARSET;
 	
-	public void createFile(String name) {
-		file = new File(new String("../bitter."), name);
-		FileInputStream input = new FileInputStream(name);		
-		
+	public FileIO() {
+		CHARSET = Charset.forName("US-ASCII");
+	}
+	public void writeToFile (String text, File file) {
+		try (BufferedWriter writer = File.newBufferedWriter(file, CHARSET)) {
+		    writer.write(text, 0, text.length());
+		} catch (IOException x) {
+		    System.err.format("IOException: %s%n", x);
+		}
+	}
+	public void readFromFile (File file) {
+		try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+		    String line = null;
+		    while ((line = reader.readLine()) != null) {
+		        System.out.println(line);
+		    }
+		} catch (IOException x) {
+		    System.err.format("IOException: %s%n", x);
+		}
 	}
 }
