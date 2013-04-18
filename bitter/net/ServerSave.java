4fe96376be6c3514f48c25515a745641f48cf63a
Alfandorachk <spiffpneumoyakk@gmail.com>
2013-04-18 06:29:30 -0400

    worked on ServerSave

--- bitter/net/ServerSave.java
+++ bitter/net/ServerSave.java
@@ -9,9 +9,35 @@ package bitter.net;
  * @author ryanogorman
  */
 public class ServerSave {
+
+	public static void ServerSaver(MessageListHash mHash, UserHashMap uHash,
+			UserLookupTable lTable) {
+		FileOutputStream f_out = new FileOutputStream("listhash.data");
+		ObjectOutput Stream obj_out = new ObjectOutputStream(f_out);
+		obj_out.writeObject(mHash);
+
+		f_out.close();
+		obj_out.close();
+
+		f_out = new FileOutputStream("userhash.data");
+		ObjectOutput Stream obj_out = new ObjectOutputStream(f_out);
+		obj_out.writeObject(uHash);
+		
+		f_out.close();
+		obj_out.close();
+
+		f_out = new FileOutputStream("lookuptable.data");
+		ObjectOutput Stream obj_out = new ObjectOutputStream(f_out);
+		obj_out.writeObject(lTable);
+		
+		f_out.close();
+		obj_out.close();
+	}
+/*
     FileOutputStream f_out = new FileOutputStream("myobject.data");
     
     ObjectOutput Stream obj_out = new ObjectOutputStream(f_out);
     
     obj_out.writeObject(myObject);
+*/
 }
